2d4f8f5296b5ea1a2c6f1af6f446b802afe24562
Denuwa777 <devilprogramer0705209559@gmail.com>
2024-10-05 18:23:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const {
 const path = require('path')
 const msgRetryCounterCache = new NodeCache()
 const prefix = '.'
-const ownerNumber = ['94725881990']
+const ownerNumber = ['94722336454']
 //===================SESSION============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
     if (config.SESSION_ID) {
@@ -479,7 +479,7 @@ async function connectToWA() {
     })
 }
 app.get("/", (req, res) => {
-    res.send("📟 Nebula Working successfully!");
+    res.send("📟 DENUWAN-MD Working successfully!");
 });
 app.listen(port, () => console.log(`Nebula Server listening on port http://localhost:${port}`));
 setTimeout(async () => {
